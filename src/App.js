6bb9d70143e4421875649be30405bb6d6e2895d9
Andy Nguyen <apnguyen11@yahoo.com>
2019-12-18 20:18:50 -0600

    functional form that allows you to post text to a blog of posts taken from jsonplaceholder

--- src/App.js
+++ src/App.js
@@ -13,15 +13,11 @@ function App() {
   return (
     <Provider store={store}>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+     
       <PostForm/>
       <hr />
       <Posts/>
-      </header>
+     
     </div>
     </Provider>
   );
